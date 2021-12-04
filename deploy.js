1930007b7f02471c18fb6262765e8bae995ded56
Eduardo Santiago <91878554+PensoGlide@users.noreply.github.com>
2021-12-04 12:37:11 +0000

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,8 +3,8 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } =  require('./compile');
 const provider = new HDWalletProvider(
-    'layer patrol draft lumber garage theme boost flush resist obey slot picnic', // Wallet Mnemonic
-    'https://rinkeby.infura.io/v3/93e37a31992643abb62ca64f80c266f5'               // Infura Rinkeby Test Network address
+    '', // Wallet Mnemonic
+    ''  // Infura Rinkeby Test Network address
 );
 const web3 = new Web3(provider);
 
